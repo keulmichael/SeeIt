c7d74eb7c5926805b6d75d0ca696c35d4a273442
Host <host.michael@gmail.com>
2015-04-29 17:42:39 +0200

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -1,5 +1,6 @@
 var settings;
 
+
 function onLoadCamera() {
 	
 	var networkState = navigator.network.connection.type;
@@ -38,7 +39,33 @@ function onDeviceReady() {
     document.addEventListener("offline", onOffline, false)
 }
 
+function onBackPress(e) {
+    
+    if($.mobile.activePage.is("#home_page")){
+        
+        e.preventDefault();
+        removeTemporaryFiles();
+        navigator.app.exitApp();
+    }
+    else {
+        
+        if ($.mobile.activePage.is("#settings_page")) {
+            restoreSettings();
+        }
+        
+        navigator.app.backHistory();
+    }
+}
 
+function removeTemporaryFiles() {
+    
+    if (isIOS()) {
+        
+    }
+    
+    function onSuccess() { }
+    function onError(message) { }
+}
 
 function onCapture(e) {
 if (settings.positionPaysage==false)
@@ -84,7 +111,6 @@ if (acceleration.x>1 || acceleration.x<-1 && acceleration.y<9 && acceleration.z>
 
 		}
 	};
-
 
 function accelerometerErrorPortrait() {
     alert('Veuillez activer l\'accéléromètre');
@@ -196,5 +222,124 @@ var options = new FileUploadOptions();
 }
 }
 
-
 function onCaptureError(message) {alert(message); }
+
+function applySettings() {
+    
+    var settingsBatch = getElement("settings_form");
+    if (settingsBatch == null) {
+        return;
+    }
+        
+    var newQuality = parseInt(settingsBatch.elements["quality_input"].value, 10);
+    if (!isNaN(newQuality) && (newQuality <= 100) && (newQuality >= 0)) {
+        settings.quality = newQuality;
+    }
+        
+    var newWidth = parseInt(settingsBatch.elements["width_input"].value, 10);
+    if (!isNaN(newWidth) && (newWidth <= 1500) && (newWidth >= 50)) {
+        settings.targetWidth = newWidth;
+    }
+    
+    var newHeight = parseInt(settingsBatch.elements["height_input"].value, 10);
+    if (!isNaN(newHeight) && (newHeight <= 1500) && (newHeight >= 50)) {
+        settings.targetHeight = newHeight;
+    }
+    
+    settings.allowEdit = settingsBatch.elements["edit_input"].checked;
+    settings.correctOrientation = settingsBatch.elements["orient_input"].checked;
+    settings.saveToPhotoAlbum = (settingsBatch.elements["save_input"].options[settingsBatch.elements["save_input"].selectedIndex].value == "true") ? true : false;
+    settings.encodingType = parseInt(settingsBatch.elements["encod_input"].options[settingsBatch.elements["encod_input"].selectedIndex].value, 10);
+    settings.mediaType = parseInt(settingsBatch.elements["media_input"].options[settingsBatch.elements["media_input"].selectedIndex].value, 10);
+    
+    fillSettingsInfo("settings_info");
+}
+
+function restoreSettings() {
+    
+    $("#quality_input").val(settings.quality).slider("refresh");
+    $("#width_input").val(settings.targetWidth).slider("refresh");
+    $("#height_input").val(settings.targetHeight).slider("refresh");
+    
+    if (settings.allowEdit) {
+        $("#edit_input").attr("checked", true).checkboxradio("refresh");
+    } else {
+        $("#edit_input").removeAttr("checked").checkboxradio("refresh");
+    }
+    
+    if (settings.correctOrientation) {
+        $("#orient_input").attr("checked", true).checkboxradio("refresh");
+    } else {
+        $("#orient_input").removeAttr("checked").checkboxradio("refresh");
+    }
+    
+    var saveSwitch = $("#save_input");
+    saveSwitch[0].selectedIndex = ((settings.saveToPhotoAlbum === true) ? 1 : 0);
+    saveSwitch.slider("refresh");
+    
+    $("#encod_input").val(settings.encodingType).selectmenu("refresh");
+    $("#media_input").val(settings.mediaType).selectmenu("refresh");
+}
+
+function getTargetId(event, tagName) {
+    var target = (event.target.tagName == tagName)
+                    ? event.target
+                    : $(event.target).closest(tagName)[0]
+    return target.id;
+}
+
+function getElement(element) {
+    
+    if(typeof(element) == "string") {
+    
+        element = document.getElementById(element);
+    }
+    
+    return element;
+}
+
+function fillSettingsInfo(infoDivName) {
+    
+    var settingsBatch = getElement("settings_form");
+    if (settingsBatch == null) {
+        return;
+    }
+    
+    var settingsInfo = getElement(infoDivName);
+    if (typeof Camera === "undefined") {
+        settingsInfo.innerHTML = "<h3 style='text-decoration: underline;'>The Cordova Camera API is inaccessible</h3>";
+    }
+    else {
+        settingsInfo.innerHTML = "";
+    }
+    
+    if (settingsInfo != null) {
+        settingsInfo.innerHTML += "<h3>Settings: </h3>" +
+                                 "<table>" +
+                                 "<tr><td class='bh'>Editing options: </td></tr>" +
+                                 "<tr><td class='bi'>Quality:</td><td>" + settings.quality + " of 100</td></tr>" +
+                                 "<tr><td class='bi'>Target picture width:</td><td>" + settings.targetWidth + " px</td></tr>" +
+                                 "<tr><td class='bi'>Target picture height:</td><td>" + settings.targetHeight + " px</td></tr>" +
+                                 "<tr><td class='bi'>Allow picture zoom and crop:</td><td>" + ((settings.allowEdit == true) ? "Yes" : "No") + "</td></tr>" +
+                                 "<tr><td class='bi'>Correct orientation:</td><td>" + ((settings.correctOrientation == true) ? "Yes" : "No") + "</td></tr>" +
+                                 "<tr><td class='bh'>Saving options: </td></tr>" +
+                                 "<tr><td class='bi'>Target encoding type:</td><td>" + settingsBatch.elements["encod_input"].options[settings.encodingType].innerHTML + "</td></tr>" +
+                                 "<tr><td class='bi'>Save to Photo Album:</td><td>" + ((settings.saveToPhotoAlbum == true) ? "Yes" : "No") + "</td></tr>" +
+                                 "<tr><td class='bh'>Opening options: </td></tr>" +
+                                 "<tr><td class='bi'>Browse on open:</td><td>" + settingsBatch.elements["media_input"].options[settings.mediaType].innerHTML + "</td></tr>" +
+                                 "</table>";
+    }
+}
+
+function isIOS() {
+
+    var iDevices = ["iPad", "iPhone", "iPod"];
+
+    for (var i = 0; i < iDevices.length ; i++ ) {
+        
+        if( navigator.platform.indexOf(iDevices[i]) !== -1){
+            return true;
+        }
+    }
+    return false;
+}
