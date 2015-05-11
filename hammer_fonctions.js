4d57db864da3f453768c7be8ce8e7e8e5d997888
Host <host.michael@gmail.com>
2015-05-11 15:14:37 +0200

    Update hammer_fonctions.js

--- hammer_fonctions.js
+++ hammer_fonctions.js
@@ -5,8 +5,10 @@ function hammer_excecute(id)
             window.setTimeout(callback, 1000 / 60);
         };
     })();
-    var log = document.getElementById("log");
+
     var el = document.getElementById(id);
+    var ele = id.split("_"); var loge="logphotoaccueil_"+ele[1];
+    var log = document.getElementById(v);
     var START_X = 0;
     var START_Y = 0;
     var ticking = false;
@@ -133,135 +135,4 @@ function hammer_excecute(id)
     
     }
     
-    
-    
-    var reqAnimationFrame = (function () {
-        return window[Hammer.prefixed(window, 'requestAnimationFrame')] || function (callback) {
-            window.setTimeout(callback, 1000 / 60);
-        };
-    })();
-    var logphotoaccueil = document.getElementById("logphotoaccueil");
-    var elphotoaccueil = document.getElementById("photoaccueil");
-    var START_X = 0;
-    var START_Y = 0;
-    var ticking = false;
-    var transform;
-    var timer;
-    var mc = new Hammer.Manager(elphotoaccueil);
-    mc.add(new Hammer.Pan({ threshold: 0, pointers: 0 }));
-    mc.add(new Hammer.Swipe()).recognizeWith(mc.get('pan'));
-    mc.add(new Hammer.Rotate({ threshold: 0 })).recognizeWith(mc.get('pan'));
-    mc.add(new Hammer.Pinch({ threshold: 0 })).recognizeWith([mc.get('pan'), mc.get('rotate')]);
-    mc.add(new Hammer.Tap({ event: 'doubletap', taps: 2 }));
-    mc.add(new Hammer.Tap());
-    mc.on("panstart panmove", onPan);
-    mc.on("rotatestart rotatemove", onRotate);
-    mc.on("pinchstart pinchmove", onPinch);
-    mc.on("swipe", onSwipe);
-    mc.on("tap", onTap);
-    mc.on("doubletap", onDoubleTap);
-    mc.on("hammer.input", function(ev) {
-        if(ev.isFinal) {
-            resetElement();
-        }
-    });
-    function resetElement() {
-        elphotoaccueil.className = 'animate';
-        transform = {
-            translate: { x: START_X, y: START_Y },
-            scale: 1,
-            angle: 0,
-            rx: 0,
-            ry: 0,
-            rz: 0
-        };
-        requestElementUpdate();
-        if (logphotoaccueil.textContent.length > 2000) {
-            logphotoaccueil.textContent = logphotoaccueil.textContent.substring(0, 2000) + "...";
-        }
-    }
-    function updateElementTransform() {
-        var value = [
-                    'translate3d(' + transform.translate.x + 'px, ' + transform.translate.y + 'px, 0)',
-                    'scale(' + transform.scale + ', ' + transform.scale + ')',
-                    'rotate3d('+ transform.rx +','+ transform.ry +','+ transform.rz +','+  transform.angle + 'deg)'
-        ];
-        value = value.join(" ");
-        elphotoaccueil.textContent = value;
-        elphotoaccueil.style.webkitTransform = value;
-        elphotoaccueil.style.mozTransform = value;
-        elphotoaccueil.style.transform = value;
-        ticking = false;
-    }
-    function requestElementUpdate() {
-        if(!ticking) {
-            reqAnimationFrame(updateElementTransform);
-            ticking = true;
-        }
-    }
-    function logEvent(str) {
-        //logphotoaccueil.insertBefore(document.createTextNode(str +"\n"), logphotoaccueil.firstChild);
-    }
-    function onPan(ev) {
-        elphotoaccueil.className = '';
-        transform.translate = {
-            x: START_X + ev.deltaX,
-            y: START_Y + ev.deltaY
-        };
-        requestElementUpdate();
-        logEvent(ev.type);
-    }
-    var initScale = 1;
-    function onPinch(ev) {
-        if(ev.type == 'pinchstart') {
-            initScale = transform.scale || 1;
-        }
-        elphotoaccueil.className = '';
-        transform.scale = initScale * ev.scale;
-        requestElementUpdate();
-        logEvent(ev.type);
-    }
-    var initAngle = 0;
-    function onRotate(ev) {
-        if(ev.type == 'rotatestart') {
-            initAngle = transform.angle || 0;
-        }
-        elphotoaccueil.className = '';
-        transform.rz = 1;
-        transform.angle = initAngle + ev.rotation;
-        requestElementUpdate();
-        logEvent(ev.type);
-    }
-    function onSwipe(ev) {
-        var angle = 50;
-        transform.ry = (ev.direction & Hammer.DIRECTION_HORIZONTAL) ? 1 : 0;
-        transform.rx = (ev.direction & Hammer.DIRECTION_VERTICAL) ? 1 : 0;
-        transform.angle = (ev.direction & (Hammer.DIRECTION_RIGHT | Hammer.DIRECTION_UP)) ? angle : -angle;
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-            resetElement();
-        }, 300);
-        requestElementUpdate();
-        logEvent(ev.type);
-    }
-    function onTap(ev) {
-        transform.rx = 1;
-        transform.angle = 25;
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-            resetElement();
-        }, 200);
-        requestElementUpdate();
-        logEvent(ev.type);
-    }
-    function onDoubleTap(ev) {
-        transform.rx = 1;
-        transform.angle = 80;
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-            resetElement();
-        }, 500);
-        requestElementUpdate();
-        logEvent(ev.type);
-    }
-    resetElement();
+  
