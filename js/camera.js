2ab03cff65f0b9de9de4bfbe68fb4c829e8d833b
Host <host.michael@gmail.com>
2015-06-05 13:05:58 +0200

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -187,7 +187,10 @@ require(["dojo/request"], function(request){
 var num = document.getElementById("num").value;	
 var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 geocoder.geocode({'latLng': latlng}, function(results, status) {
+	if (status == google.maps.GeocoderStatus.OK) {
+if (results[1]) {
       request.get('http://www.appliseeit.com/mobile/record_gps.php?num='+num+'&x='+position.coords.latitude+'&y='+position.coords.longitude+'&adress='+results[1].formatted_address).then(function(response276){ }) ;
+      }}
 })	
 
     })
